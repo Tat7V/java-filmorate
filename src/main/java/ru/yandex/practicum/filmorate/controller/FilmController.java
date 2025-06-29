bc32b225906a8c5173eb8f746545d2351ddceefc
Tatyana Volodina <volodina.tv@yandex.ru>
2025-06-29 14:54:18 +0300

    добавлен функционал создания, добавления, обновления и валидации пользователей и фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,51 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+    private long idCounter = 1;
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        validateFilm(film);
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        log.info("Был добавлен фильм {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        if (film.getId() == null || !films.containsKey(film.getId())) {
+            throw new ValidationException("Фильм с таким id не существует");
+        }
+        validateFilm(film);
+        films.put(film.getId(), film);
+        log.info("Был обновлен фильм: {}", film);
+        return film;
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private long idCounter = 1;
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        user.setId(idCounter++);
+        users.put(user.getId(), user);
+        log.info("Добавлен пользователь: {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        if (user.getId() == null || !users.containsKey(user.getId())) {
+            throw new ValidationException("Пользователь с таким id не существует");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Обновлен пользователь: {}", user);
+        return user;
+    }
+}
